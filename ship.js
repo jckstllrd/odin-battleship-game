3b2c0300f7a14bcca283a30c6b78aaf7280e85d9
jckstllrd <jackstallard17@gmail.com>
2026-01-14 17:36:48 +0000

    Add Ship class and testing suite

---
+++ ship.js
@@ -0,0 +1,18 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hits = this.hits + 1;
+  }
+
+  isSunk() {
+    if (this.hits >= this.length) this.sunk = true;
+    return this.sunk;
+  }
+}
+
+export { Ship };
--- ship.test.js
+++ ship.test.js
@@ -1,3 +1,27 @@
-test("Creating a Ship returns an object containing length, no. hits, ifSunk", () => {
-  expect(new Ship(3)).toMatchObject({ length: 3, hits: 0, ifSunk: false });
+import { Ship } from './ship'
+
+
+it("returns a ship object containing length, no. hits and ifSunk", () => {
+  const ship = new Ship(3);
+  console.log(ship);
+  
+  expect(ship.length).toBe(3);
+  expect(ship.hits).toBe(0)
+  expect(ship.sunk).not.toBeTruthy()
 });
+
+it("increases the number of hits on a new object by +1", () => {
+  const ship = new Ship(3);
+  expect(ship.hits).toBe(0)
+  ship.hit()
+  expect(ship.hits).toBe(1);
+})
+
+it("outputs true once the ship isSunk()", () => {
+  const ship = new Ship(3);
+  expect(ship.isSunk()).not.toBeTruthy()
+  ship.hit()
+  ship.hit()
+  ship.hit()
+  expect(ship.isSunk()).toBeTruthy()
+})
